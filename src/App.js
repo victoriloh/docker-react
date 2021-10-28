8ad057f8ddb4cb08dd7aad7238e3ef6d81573599
victoriloh <victor@bundle.africa>
2021-10-28 09:09:53 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello my name is victor. I'm a DevOps Engineer. Bye there
         </p>
         <a
           className="App-link"
